7f3900656733f4eb5d137636d44f237abe670074
syspori <sitotheyb@gmail.com>
2024-06-05 00:17:45 +0900

    third commit

--- src/github/Trial01.java
+++ src/github/Trial01.java
@@ -4,5 +4,6 @@ public class Trial01 {
 	public static void main(String[] args) {
 		System.out.println("Github is easy and useful!");
 		System.out.println("Wow! That's great!");
+		System.out.println("0605 12:15");
 	}
 }
